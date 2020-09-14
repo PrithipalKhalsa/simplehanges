f40572e551b37db44e9a46ce57c34d0e399e21e7
prithipal khalsa <prithipalpsk@gmail.com>
2020-09-13 19:07:30 -0700

    Updates

---
+++ precache-manifest.cc8a0eb1d4c2d5e05974801873fb45e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f7b88de22b3e5eb7cb1bc2efd1fdaca",
+    "url": "/simplehangs/index.html"
+  },
+  {
+    "revision": "ff7e06f1fb534315404f",
+    "url": "/simplehangs/static/css/main.e9cdcbd5.chunk.css"
+  },
+  {
+    "revision": "4e3042b3a21b1b5ef204",
+    "url": "/simplehangs/static/js/2.85b8ade9.chunk.js"
+  },
+  {
+    "revision": "c5930055a21c439b1783da4e9887b5d2",
+    "url": "/simplehangs/static/js/2.85b8ade9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff7e06f1fb534315404f",
+    "url": "/simplehangs/static/js/main.bec8eac3.chunk.js"
+  },
+  {
+    "revision": "1b9da010f53304da0a43",
+    "url": "/simplehangs/static/js/runtime-main.ff68ebb5.js"
+  }
+]);
