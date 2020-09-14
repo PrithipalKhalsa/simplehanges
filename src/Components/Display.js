d853d3d1cbab966c013d849375b279aba3963c34
prithipal khalsa <prithipalpsk@gmail.com>
2020-09-14 10:48:11 -0700

    sounds

---
+++ src/Components/Display.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+
+// import './Components.css';
+import Controles from './Controles';
+import s2 from './sounds/sound0.wav';
+import s1 from './sounds/sound1.wav';
+import s0 from './sounds/sound2.wav';
+
+
+const Display = ({ mute, setMute, sets,total, minutes,seconds,j,rounds,displayRef,i}) => {
+  var sound=[s0,s1,s2]
+
+  const [toggle, setToggle] = useState(false);
+  const [childMute, setChildMute] = useState(mute);
+  const [audio] = useState(new Audio(sound[i]));
+  const [playing, setPlaying] = useState(true);
+  const divStyle = {
+  animation: 'grow '+ total+'s infinite',
+};
+
+useEffect(() => {
+     playing&&!mute ? audio.play() : audio.pause();
+
+     return ()=>{setMute(false); console.log("DONE!"+mute)}
+   },
+   [playing]
+ );
+
+
+ useEffect(() => {
+   audio.addEventListener('ended', () => setPlaying(false));
+   return () => {
+     audio.removeEventListener('ended', () => setPlaying(false));
+   };
+ }, []);
+
+  return (
+    <div className="display-wrapper" >
+    {!childMute
+    ?<div className="icono-volumeMedium vol" onClick={()=>setChildMute(!childMute)}></div>
+    :<div className="icono-volume vol" onClick={()=>setChildMute(!childMute)}></div>
+    }
+      <div className="score">{j}/{rounds} Reps</div>
+      <div className={['numbers-wrapper', 'stage-'+i].join(' ')}>
+
+        { minutes==undefined ?
+          <div className="numbers" >{seconds} </div>
+        :
+        seconds>10?  <div className="numbers"> {minutes}:{seconds}</div>
+        :
+        <div className="numbers"> {minutes}:0{seconds}</div>
+       }
+       <div  className={!toggle?"circle":"circle paused"} style={divStyle}></div>
+
+      </div>
+      <div className="sets">{sets} Sets</div>
+      { i!=-1&&
+         <Controles toggle={toggle} setToggle={setToggle} displayRef={displayRef}
+/>
+       }
+  </div>
+  );
+}
+
+export default Display;
---
+++ src/Components/Repeaters.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+import Countdown from 'react-countdown';
+
+ import './Components.css';
+
+import Display from './Display';
+
+
+const Repeaters = ({time,map,rounds}) => {
+  const [toggle, setToggle] = useState(false);
+  const [mute, setMute] = useState(true);
+  var displayRef = React.createRef();
+  var i=-1
+  var j=1
+  var sets=0
+  // var mute=false
+  useEffect(() => {
+      console.log("rep render")
+     }
+   );
+   useEffect(() => {
+       console.log("rep update "+mute)
+      }
+    );
+
+const Completionist = React.memo(() =>   {
+  if(i==1&&j<rounds){
+    j++
+    i=-1
+  }
+  else if(i==2){
+    sets++
+    j=1
+    i=-1
+  }
+  i++
+   return <Countdown date={Date.now() + map[i]*1000} zeroPadTime={1} renderer={renderer}   ref={displayRef}/>
+
+ })
+
+
+const renderer = ({hours, minutes, seconds, completed }) => {
+  if (completed)
+        return <Completionist />;
+  else {
+    // Render a countdown
+    if(minutes>0)
+    return <Display mute={mute} setMute={setMute} sets={sets} total={map[i]} toggle={toggle} minutes={minutes} seconds={seconds} j={j} rounds={rounds} displayRef={displayRef} i={i}/>;
+    else
+      return <Display mute={mute} setMute={setMute} sets={sets} total={map[i]} toggle={toggle} seconds={seconds} j={j} rounds={rounds} displayRef={displayRef} i={i}/>;
+  }
+};
+
+  return (
+    <div className="inner-app-wrapper">
+
+    <Countdown
+    date={Date.now() + 5000}
+    renderer={renderer}/>
+  </div>
+
+  );
+}
+
+export default Repeaters;
